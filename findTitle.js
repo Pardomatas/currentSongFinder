aae80c72a88aaaaacd8b4446ce8eecd889c6251a
Mychal <mychal.wilson@gmail.com>
2020-04-06 11:16:31 -0400

    get title and auto download, and overwrite if txt exists

--- findTitle.js
+++ findTitle.js
@@ -1,3 +1,40 @@
-let title = document.getElementById('info');
+let checkExist = setInterval(function() {
+    let titleElement = document.getElementsByTagName("title")[0].textContent;
+    if (titleElement != "YouTube") {
 
-console.log(title);
+        let title = titleElement.substring(0, titleElement.length - 9);
+
+        console.log(title);
+
+        console.save(title, "songTitle");
+
+        clearInterval(checkExist);
+    }
+ }, 100);
+
+ (function(console){
+
+    console.save = function(data, filename){
+
+        if(!data) {
+            console.error('Console.save: No data')
+            return;
+        }
+
+        if(!filename) filename = 'songTitle.txt'
+
+        if(typeof data === "object"){
+            data = JSON.stringify(data, undefined, 4)
+        }
+
+        var blob = new Blob([data], {type: 'text/plain'}),
+            e    = document.createEvent('MouseEvents'),
+            a    = document.createElement('a')
+
+        a.download = filename
+        a.href = window.URL.createObjectURL(blob)
+        a.dataset.downloadurl =  ['text/plain', a.download, a.href].join(':')
+        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+        a.dispatchEvent(e)
+    }
+})(console)

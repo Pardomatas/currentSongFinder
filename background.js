e67ca084c8cb30334bd6e40d111f64bd509ab895
Mychal <mychal.wilson@gmail.com>
2020-05-10 16:00:24 -0400

    beginning work for enable/disable button

--- background.js
+++ background.js
@@ -1,17 +1,23 @@
-chrome.downloads.onDeterminingFilename.addListener(function (item, suggest) {
-	suggest({filename: "..", conflictAction: "overwrite"});
-});
+// To set 
+chrome.storage.local.set({'isEnabled':false});
 
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {	
-	if(details.frameId === 0) {
-        // Fires only when details.url === currentTab.url
-        chrome.tabs.get(details.tabId, function(tab) {
-            if(tab.url === details.url) {
-				chrome.tabs.executeScript(null, {file: "findTitle.js"});
-            }
-        });
-    }
-});
+function click(e) {
+	// To get
+	chrome.storage.local.get('isEnabled', function(data){
+		console.log(data.isEnabled); 
+		
+		let isEnabled = data.isEnabled;
+
+		if(isEnabled) {
+			chrome.browserAction.setBadgeText({text: "Off"});
+		} else if(!isEnabled) {
+			chrome.browserAction.setBadgeText({text: "ON"});
+		}
+		isEnabled = !isEnabled;
+		chrome.storage.local.set({'isEnabled':isEnabled});
+	})
+}
+
 
 function clearDownloads() {
 	setTimeout(function() {
@@ -19,10 +25,40 @@ function clearDownloads() {
 	}, 1000)
 };
 
-chrome.downloads.onChanged.addListener(function (e) {
-	if (typeof e.state !== "undefined") {
-		if (e.state.current === "complete") {
-			clearDownloads();
-		}
-	}
+chrome.downloads.onDeterminingFilename.addListener(function (item, suggest) {
+	suggest({filename: "songTitle.txt", conflictAction: "overwrite"});	
 });
+	
+chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {	
+	chrome.storage.local.get('isEnabled', function(data){
+
+		if(data.isEnabled) {
+
+			if(details.frameId === 0) {
+				// Fires only when details.url === currentTab.url
+				chrome.tabs.get(details.tabId, function(tab) {
+					if(tab.url === details.url) {
+						chrome.tabs.executeScript(null, {file: "findTitle.js"});
+					}
+				});
+			}
+		}
+	})
+});
+	
+chrome.downloads.onChanged.addListener(function (event) {
+	chrome.storage.local.get('isEnabled', function(data){
+		
+		if(data.isEnabled) {
+
+			if (typeof event.state !== "undefined") {
+				if (event.state.current === "complete") {
+					clearDownloads();
+				}
+			}
+		}
+
+	})
+});
+
+chrome.browserAction.onClicked.addListener(click);
